f7de28ad7bed48b17dc645badae85306089d8e16
Luca Colagrande <luca.colagrande3@gmail.com>
2026-03-13 14:52:09 +0100

    hw: Separate initial slot update logic

--- hw/schnizo/src/schnizo_res_stat_slot.sv
+++ hw/schnizo/src/schnizo_res_stat_slot.sv
@@ -290,155 +290,26 @@ module schnizo_res_stat_slot import schnizo_pkg::*; #(
   // Slot selection //
   ////////////////////
 
-  // This stage performs an initial state-dependent slot update.
-
-  // The initial operand values when accepting a new instruction
-  rss_operand_t op_a_lcp1;
-  rss_operand_t op_b_lcp1;
-  rss_operand_t op_c_lcp1;
-
-  assign op_a_lcp1 = '{
-    producer:             disp_req_i.producer_op_a.producer,
-    is_produced:          disp_req_i.producer_op_a.valid,
-    is_from_current_iter: disp_req_i.producer_op_a.valid,
-    value:                disp_req_i.fu_data.operand_a,
-    is_valid:             !disp_req_i.producer_op_a.valid,
-    requested:            1'b0
-  };
-
-  assign op_b_lcp1 = '{
-    producer:             disp_req_i.producer_op_b.producer,
-    is_produced:          disp_req_i.producer_op_b.valid,
-    is_from_current_iter: disp_req_i.producer_op_b.valid,
-    value:                disp_req_i.fu_data.operand_b,
-    is_valid:             !disp_req_i.producer_op_b.valid,
-    requested:            1'b0
-  };
-
-  assign op_c_lcp1 = '{
-    producer:             disp_req_i.producer_op_c.producer,
-    is_produced:          disp_req_i.producer_op_c.valid,
-    is_from_current_iter: disp_req_i.producer_op_c.valid,
-    value:                disp_req_i.fu_data.imm,
-    is_valid:             !disp_req_i.producer_op_c.valid,
-    requested:            1'b0
-  };
-
-  // Array to simplify initial operand assignment
-  rss_operand_t [2:0] ops_lcp1;
-  assign ops_lcp1[0] = op_a_lcp1;
-  assign ops_lcp1[1] = op_b_lcp1;
-  assign ops_lcp1[2] = op_c_lcp1;
-
-  // Initial value of the slot upon accepting a new instruction
-  rs_slot_t slot_lcp1;
-  always_comb begin
-    slot_lcp1 = '{
-      is_occupied:          1'b1,
-      consumer_count:       '0,
-      consumed_by:          '0,
-      alu_op:               disp_req_i.fu_data.alu_op,
-      lsu_op:               disp_req_i.fu_data.lsu_op,
-      fpu_op:               disp_req_i.fu_data.fpu_op,
-      // Duplicate logic for is_store. Once in LSU and once here.
-      // TODO: Optimize by passing this to the LSU instead of regenerating it inside the LSU?
-      is_store:             (disp_req_i.fu_data.fu == STORE) &&
-                            (disp_req_i.fu_data.fpu_op inside {LsuOpStore, LsuOpFpStore}),
-      lsu_size:             disp_req_i.fu_data.lsu_size,
-      fpu_fmt_src:          disp_req_i.fu_data.fpu_fmt_src,
-      fpu_fmt_dst:          disp_req_i.fu_data.fpu_fmt_dst,
-      fpu_rnd_mode:         disp_req_i.fu_data.fpu_rnd_mode,
-      // We must set the result iteration flag to 1. It gets toggled when writing the first result.
-      result:               rss_result_t '{
-        value:     '0,
-        is_valid:  1'b0,
-        iteration: 1'b1
-      },
-      instruction_iter:     1'b0,
-      dest_id:              disp_req_i.tag.dest_reg,
-      dest_is_fp:           disp_req_i.tag.dest_reg_is_fp,
-      do_writeback:         1'b0,
-      operands:             '0
-    };
-
-    // Operands must be assigned depending on the number we have
-    for (int op = 0; op < NofOperands; op++) begin
-      slot_lcp1.operands[op] = ops_lcp1[op];
-    end
-  end
-
-  // Initial value of the slot upon accepting an instruction in LCP2.
-  // Now all operand producers should be known, so we can update the missing producer information.
-  // We also now know which instruction is the last in the loop to write to a certain register.
-  // We can therefore also update the `do_writeback` flag.
-  rs_slot_t slot_lcp2;
-  always_comb begin
-    slot_lcp2 = slot_q;
-
-    // Update producers if there is not yet one set.
-    // TODO(colluca): could we also update the producer for operands which already
-    // have a producer? Probably yes, then this would be an energy-saving optimization.
-    // Test this so we can better document it.
-    if (!slot_lcp2.operands[0].is_produced) begin
-      slot_lcp2.operands[0].producer    = disp_req_i.producer_op_a.producer;
-      slot_lcp2.operands[0].is_produced = disp_req_i.producer_op_a.valid;
-      slot_lcp2.operands[0].is_valid    = !disp_req_i.producer_op_a.valid;
-      slot_lcp2.operands[0].value       = disp_req_i.fu_data.operand_a;
-    end
-    if (!slot_lcp2.operands[1].is_produced) begin
-      slot_lcp2.operands[1].producer    = disp_req_i.producer_op_b.producer;
-      slot_lcp2.operands[1].is_produced = disp_req_i.producer_op_b.valid;
-      slot_lcp2.operands[1].is_valid    = !disp_req_i.producer_op_b.valid;
-      slot_lcp2.operands[1].value       = disp_req_i.fu_data.operand_b;
-    end
-    if (NofOperands >= 3) begin
-      if (!slot_lcp2.operands[2].is_produced) begin
-        slot_lcp2.operands[2].producer    = disp_req_i.producer_op_c.producer;
-        slot_lcp2.operands[2].is_produced = disp_req_i.producer_op_c.valid;
-        slot_lcp2.operands[2].is_valid    = !disp_req_i.producer_op_c.valid;
-        slot_lcp2.operands[2].value       = disp_req_i.fu_data.imm;
-      end
-    end
-    // Set the writeback flag if we are the last RSS writing to this destination
-    // TODO(colluca): couldn't we have the dispatcher calculate `do_writeback` instead?
-    // This way we don't need to store it in the cut. It may increase the critical path
-    // if the critical path is before the cut.
-    if ((own_producer_id_i == disp_req_i.current_producer_dest.producer) &&
-        disp_req_i.current_producer_dest.valid) begin
-      slot_lcp2.do_writeback = 1'b1;
-    end
-  end
-
   // Initial state-dependent slot update
   rs_slot_t selected_slot;
 
-  // INFO(soderma):
-  // We can use the regular state. The reason pascal probably did this is because of multi cycle updates.
-  // so that you don't overwrite the dispatch request. But the dispatch request valid signal is only valid for one cycle anyway in LCP1 and LCP2
-  // reason being is that all operands are ready and the instruction can be immediately issued.
-  always_comb begin : slot_selection
-    // Update the slot depending on the state.
-    selected_slot = slot_q;
-    unique case (loop_state_i)
-      LoopLcp1: begin
-        if (disp_req_valid_i) begin
-          selected_slot = slot_lcp1; // Load the new instruction
-        end
-      end
-      LoopLcp2: begin
-        if (disp_req_valid_i) begin
-          // Update producers if there is not yet one set.
-          selected_slot = slot_lcp2;
-        end
-      end
-      default: ;
-    endcase
-
-    // Slot initialization has highest priority
-    if (restart_i) begin
-      selected_slot = slot_reset_state;
-    end
-  end
+  schnizo_res_stat_slot_initial_update #(
+    .NofOperands   (NofOperands),
+    .disp_req_t    (disp_req_t),
+    .producer_id_t (producer_id_t),
+    .rs_slot_t     (rs_slot_t),
+    .rss_operand_t (rss_operand_t),
+    .rss_result_t  (rss_result_t)
+  ) i_initial_update (
+    .restart_i          (restart_i),
+    .own_producer_id_i  (own_producer_id_i),
+    .loop_state_i       (loop_state_i),
+    .disp_req_i         (disp_req_i),
+    .disp_req_valid_i   (disp_req_valid_i),
+    .slot_i             (slot_q),
+    .slot_reset_state_i (slot_reset_state),
+    .slot_o             (selected_slot)
+  );
 
   //////////////////////
   // Operand handling //
