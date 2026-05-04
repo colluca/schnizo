3597903c839b810ade6db62976c35765f1342ce9
Stefan-Odermatt <StefanOdermattET@hotmail.com>
2026-05-04 14:31:43 +0200

    experiments; Added physical register area experiment

--- experiments/schnova/rs_area/experiments.py
+++ experiments/schnova/rs_area/experiments.py
@@ -14,54 +14,25 @@ class ExperimentManager(eu.ExperimentManager):
         return experiment['hdl_params']
 
 
-#def gen_experiments():
-#    # Define axes
-#    num_slots_axis = [1, 2, 4, 8, 16, 32, 64]
-#    num_operands_axis = [3]
-#
-#    # Generate list of experiments
-#    experiments = []
-#    for num_slots in num_slots_axis:
-#            for num_operands in num_operands_axis:
-#                experiments.append({
-#                    'design': 'schnova_res_stat_synth',
-#                    'name': f'{num_slots}slots_{num_operands}operands',
-#                    'hdl_params': {
-#                        'NofRss': num_slots,
-#                        'NofOperands': num_operands
-#                    }
-#                })
-#    return experiments
-
 def gen_experiments():
     # Define axes
-    num_slots_axis = [1,2 , 4, 8, 16, 32, 64]
-    num_constants_axis = [4]
-    num_res_ports_axis = [1, 2, 3]
+    num_slots_axis = [1, 2, 4, 8, 16, 32, 64]
     num_operands_axis = [3]
-    consumer_count_axis = [64]
 
     # Generate list of experiments
     experiments = []
     for num_slots in num_slots_axis:
-        for num_constants in num_constants_axis:
-            if num_slots != 4 and num_constants != 4:
-                continue
             for num_operands in num_operands_axis:
-                for num_res_ports in num_res_ports_axis:
-                    for consumer_count in consumer_count_axis:
-                        experiments.append({
-                            'design': 'schnizo_res_stat_synth',
-                            'name': f'{num_slots}slots_{num_constants}consts_{num_res_ports}resports_{num_operands}operands',
-                            'hdl_params': {
-                                'NofRss': num_slots,
-                                'NofConstants': num_constants,
-                                'NofOperands': num_operands,
-                                'NofResRspIfs': num_res_ports,
-                                'ConsumerCount': consumer_count
-                            }
-                        })
+                experiments.append({
+                    'design': 'schnova_res_stat_synth',
+                    'name': f'{num_slots}slots_{num_operands}operands',
+                    'hdl_params': {
+                        'NofRss': num_slots,
+                        'NofOperands': num_operands
+                    }
+                })
     return experiments
+
 
 def results(dir=None):
     manager = ExperimentManager(gen_experiments(), dir=dir, parse_args=False)
