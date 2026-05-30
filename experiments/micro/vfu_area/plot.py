25ed80e7c79cd6720c286fc345911e77817a2202
jogut445 <jogut445@gmail.com>
2026-05-30 08:38:10 +0200

    Add VFU synth scripts

---
+++ experiments/micro/vfu_area/plot.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# Copyright 2026 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+import experiments
+
+GE = 0.121
+
+
+def to_ge(area_um2):
+    return area_um2 / GE
+
+
+def to_kge(area_um2):
+    return to_ge(area_um2) / 1e3
+
+
+def main():
+    df = experiments.get_results()
+
+    df['StdCellArea'] = df['synth_results'].str['qor_summary'].str['StdCellArea']
+    df['StdCellArea'] = df['StdCellArea'].map(to_kge).round(0).astype('int')
+    df['hierarchy_details'] = df['synth_results'].str['hierarchy_details']
+    df['CombArea'] = df['hierarchy_details'].map(lambda x: to_kge(x.tree.get_attr('CombArea')))
+    df['SeqArea'] = df['hierarchy_details'].map(lambda x: to_kge(x.tree.get_attr('SeqArea')))
+    df['CombArea'] = df['CombArea'].round(0).astype('int')
+    df['SeqArea'] = df['SeqArea'].round(0).astype('int')
+    df['1BitEqSeq'] = (df['synth_results'].str['multibit'].str['1BitEqSeq']).astype('int')
+    df['GE/bit'] = (1e3 * df['SeqArea'] / df['1BitEqSeq']).round(1)
+
+    df.drop(columns=['hierarchy_details'], inplace=True)
+    df.drop(columns=['synth_results'], inplace=True)
+    print(df)
+
+
+if __name__ == '__main__':
+    main()
