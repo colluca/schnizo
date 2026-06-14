d11d6488b97eeac6c18dac875549048010802e36
Luca Colagrande <luca.colagrande3@gmail.com>
2026-06-14 19:07:21 +0200

    dnn: Clean up softmax and gelu and add optimized Schnizo variants

--- sw/kernels/dnn/softmax/scripts/datagen.py
+++ sw/kernels/dnn/softmax/scripts/datagen.py
@@ -7,99 +7,70 @@
 # Viviane Potocnik <vivianep@iis.ee.ethz.ch>
 # Luca Colagrande <colluca@iis.ee.ethz.ch>
 
-import argparse
-import pathlib
-import json5
+import re
+import sys
 import torch
 
-from snitch.util.sim import data_utils
-from snitch.util.sim.data_utils import emit_license, format_struct_definition, \
-    format_array_definition, format_array_declaration, format_ifdef_wrapper
+import snitch.util.sim.data_utils as du
 
 torch.manual_seed(42)
 
-# AXI splits bursts crossing 4KB address boundaries. To minimize
-# the occurrence of these splits the data should be aligned to 4KB
 BURST_ALIGNMENT = 4096
 
 
-def golden_model(ifmap, axis):
-    softmax = torch.nn.Softmax(dim=axis)
-    return softmax(ifmap)
+class SoftmaxDataGen(du.DataGen):
 
+    def golden_model(self, ifmap, axis):
+        return torch.nn.functional.softmax(ifmap, dim=axis)
 
-def emit_header(**kwargs):
-    batch_size = kwargs['input_dim']['batch_size']
-    seq_len = kwargs['input_dim']['seq_len']
-    input_samples = kwargs['input_dim']['input_samples']
-    reduce_dim = kwargs['reduce_dim']
-    prec = kwargs['prec']
+    def infer_prec(self, funcptr):
+        bits = re.search(r'fp(\d+)', funcptr).group(1)
+        return f'FP{bits}'
 
-    torch_type = data_utils.torch_type_from_precision_t(prec)
-    ifmap = torch.randn(batch_size, seq_len, input_samples, requires_grad=False, dtype=torch_type)
+    def emit_header(self, **kwargs):
+        header = [super().emit_header()]
 
-    ofmap = golden_model(ifmap, reduce_dim)
-    ofmap = ofmap.detach().numpy()
+        batch_size = kwargs['input_dim']['batch_size']
+        seq_len    = kwargs['input_dim']['seq_len']
+        input_samples = kwargs['input_dim']['input_samples']
+        reduce_dim = kwargs['reduce_dim']
+        prec       = self.infer_prec(kwargs['funcptr'])
 
-    ifmap = data_utils.flatten(ifmap)
-    ofmap = data_utils.flatten(ofmap)
+        torch_type = du.torch_type_from_precision_t(prec)
+        ctype      = du.ctype_from_precision_t(prec)
 
-    ctype = data_utils.ctype_from_precision_t(prec)
+        ifmap = torch.randn(batch_size, seq_len, input_samples, dtype=torch_type)
+        ofmap = self.golden_model(ifmap, reduce_dim).detach()
 
-    ifmap_uid = 'ifmap'
-    ofmap_uid = 'ofmap'
+        ifmap_flat = du.flatten(ifmap)
+        ofmap_flat = du.flatten(ofmap)
 
-    layer_cfg = {
-        **kwargs['input_dim'],
-        'reduce_dim': reduce_dim,
-        'ifmap': ifmap_uid,
-        'ofmap': ofmap_uid,
-        'dtype': prec
-    }
+        ifmap_uid = 'ifmap'
+        ofmap_uid = 'ofmap'
 
-    data_str = [emit_license()]
-    data_str += [format_array_declaration(f'extern {ctype}', ifmap_uid, ifmap.shape,
-                 alignment=BURST_ALIGNMENT)]
-    data_str += [format_array_declaration(ctype, ofmap_uid, ofmap.shape,
-                 alignment=BURST_ALIGNMENT)]
-    data_str += [format_struct_definition('softmax_layer_t', 'layer', layer_cfg)]
-    data_str += [format_array_definition(ctype, ifmap_uid, ifmap,
-                 alignment=BURST_ALIGNMENT)]
-    result_def = format_array_definition(ctype, 'golden', ofmap, alignment=BURST_ALIGNMENT)
-    data_str += [format_ifdef_wrapper('BIST', result_def)]
-    data_str = '\n\n'.join(data_str)
+        layer_cfg = {
+            **kwargs['input_dim'],
+            'reduce_dim': reduce_dim,
+            'ifmap': ifmap_uid,
+            'ofmap': ofmap_uid,
+            'dtype': prec
+        }
 
-    return data_str
+        header += [du.format_array_declaration(f'extern {ctype}', ifmap_uid,
+                   ifmap_flat.shape, alignment=BURST_ALIGNMENT)]
+        header += [du.format_array_declaration(ctype, ofmap_uid,
+                   ofmap_flat.shape, alignment=BURST_ALIGNMENT)]
+        header += [du.format_struct_definition('extern const softmax_layer_t',
+                   'layer', layer_cfg)]
+        header += [du.format_array_definition(ctype, ifmap_uid,
+                   ifmap_flat, alignment=BURST_ALIGNMENT,
+                   section=kwargs.get('section'))]
+        result_def = du.format_array_definition(ctype, 'golden',
+                     ofmap_flat, alignment=BURST_ALIGNMENT)
+        header += [du.format_ifdef_wrapper('BIST', result_def)]
 
-
-def main():
-
-    parser = argparse.ArgumentParser(description='Generate data for layernorm kernel')
-    parser.add_argument(
-        "-c", "--cfg",
-        type=pathlib.Path,
-        required=True,
-        help='Select param config file kernel'
-    )
-    parser.add_argument(
-        '--section',
-        type=str,
-        help='Section to store matrices in')
-    parser.add_argument(
-        'output',
-        type=pathlib.Path,
-        help='Path of the output header file')
-    args = parser.parse_args()
-
-    # Load param config file
-    with args.cfg.open() as f:
-        param = json5.loads(f.read())
-    param['section'] = args.section
-
-    # Emit header file
-    with open(args.output, 'w') as f:
-        f.write(emit_header(**param))
+        return '\n\n'.join(header)
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(SoftmaxDataGen().main())
